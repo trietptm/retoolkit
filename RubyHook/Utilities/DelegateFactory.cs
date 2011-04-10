b9a384d02349b205349221ad31eb5e00dd2c9129
James Leskovar <jleskovar@gmail.com>
2011-04-10 19:29:16 +1000

    Updated to IronRuby 1.1.3 Updated Retoolkit to target .NET 4.0

--- RubyHook/Utilities/DelegateFactory.cs
+++ RubyHook/Utilities/DelegateFactory.cs
@@ -15,6 +15,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
 using Microsoft.Scripting.Ast;
 
